5528d47dac5076988b88e9806ef2323142a5617d
FannyLopes <62265614+FannyLopes@users.noreply.github.com>
2020-04-27 19:11:37 +0100

    Add hours slot forecast

--- src/script.js
+++ src/script.js
@@ -20,6 +20,18 @@ if (minutes < 10) {
 let showDate = document.querySelector("h2");
 showDate.innerHTML = `${today} ${hours}:${minutes}`;
 
+function formatHour(timestamp) {
+  let hours = currentDate.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = currentDate.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  return `${hours}:${minutes}`;
+}
+
 function showTemperature(response) {
   let temperature = Math.round(response.data.main.temp);
   let temperatureElement = document.querySelector("#temperature");
@@ -50,6 +62,32 @@ function showTemperature(response) {
     response.data.weather[0].description
   );
 }
+
+function showForecast(response) {
+  let forecastElement = document.querySelector("#forecast");
+  forecastElement.innerHTML = null;
+  let forecast = null;
+
+  for (let index = 0; index < 6; index++) {
+    forecast = response.data.list[index];
+    forecastElement.innerHTML = `
+   <div class="col-5">
+      <div class="forecast-card">
+        <h5>
+          ${formatHour(forecast.dt * 1000)}
+        </h5>
+          <img src="http://openweathermap.org/img/wn/${
+            forecast.weather[0].icon
+          }@2x.png" />
+        <div class="forecast-temp">
+          ${Math.round(forecast.main.temp)}º
+        </div>
+      </div>
+    </div>
+  `;
+  }
+}
+
 function showCity(event) {
   event.preventDefault();
   let cityInput = document.querySelector("#city-input");
@@ -58,6 +96,10 @@ function showCity(event) {
   let apiKey = "ad1a51d08b6686a739675f6d3e3d864c";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityInput.value}&units=metric&appid=${apiKey}`;
   axios.get(apiUrl).then(showTemperature);
+
+  apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${cityInput.value}&units=metric&appid=${apiKey}`;
+  axios.get(apiUrl).then(showForecast);
+
   cityInput.value = "";
 }
 let city = document.querySelector("#search-form");
